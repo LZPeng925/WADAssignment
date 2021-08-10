ba25a3f3c9aefc8211dc452df88a170cd3337022
LZPeng925 <lenzp-wm19@student.tarc.edu.my>
2021-08-10 18:35:32 +0800

    renew masterpage

--- Assignment/Assignment/Project.Master.designer.cs
+++ Assignment/Assignment/Project.Master.designer.cs
@@ -76,6 +76,24 @@ namespace Assignment {
         protected global::System.Web.UI.WebControls.Button btnProfile;
         
         /// <summary>
+        /// hyperlink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyperlink1;
+        
+        /// <summary>
+        /// hyperlink2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyperlink2;
+        
+        /// <summary>
         /// lblName control.
         /// </summary>
         /// <remarks>
